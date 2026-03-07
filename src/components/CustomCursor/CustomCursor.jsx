f20a6ce8a21de0f36d02f95b656cef37d72d90d4
Natasha Kaldi <kaldianastasia@gmail.com>
2026-03-07 18:05:57 +0000

    slick cursor

--- src/components/CustomCursor/CustomCursor.jsx
+++ src/components/CustomCursor/CustomCursor.jsx
@@ -6,6 +6,7 @@ function CustomCursor() {
   const [isLight, setIsLight] = useState(false)
   const [isVisible, setIsVisible] = useState(false)
   const [isHovering, setIsHovering] = useState(false)
+  const [isPressed, setIsPressed] = useState(false)
   const trailRef = useRef({ x: 0, y: 0 })
   const trailElRef = useRef(null)
   const rafRef = useRef(null)
@@ -32,8 +33,8 @@ function CustomCursor() {
       const trail = trailRef.current
       const el = trailElRef.current
       if (el) {
-        trail.x += (position.x - trail.x) * 0.15
-        trail.y += (position.y - trail.y) * 0.15
+        trail.x += (position.x - trail.x) * 0.1
+        trail.y += (position.y - trail.y) * 0.1
         el.style.left = `${trail.x}px`
         el.style.top = `${trail.y}px`
       }
@@ -56,15 +57,21 @@ function CustomCursor() {
 
     const handleMouseLeave = () => setIsVisible(false)
     const handleMouseEnter = () => setIsVisible(true)
+    const handleMouseDown = () => setIsPressed(true)
+    const handleMouseUp = () => setIsPressed(false)
 
     window.addEventListener('mousemove', handleMouseMove)
     document.addEventListener('mouseleave', handleMouseLeave)
     document.addEventListener('mouseenter', handleMouseEnter)
+    window.addEventListener('mousedown', handleMouseDown)
+    window.addEventListener('mouseup', handleMouseUp)
 
     return () => {
       window.removeEventListener('mousemove', handleMouseMove)
       document.removeEventListener('mouseleave', handleMouseLeave)
       document.removeEventListener('mouseenter', handleMouseEnter)
+      window.removeEventListener('mousedown', handleMouseDown)
+      window.removeEventListener('mouseup', handleMouseUp)
     }
   }, [checkBackground])
 
@@ -72,24 +79,36 @@ function CustomCursor() {
 
   const colorClass = isLight ? 'custom-cursor--light' : ''
   const hoverClass = isHovering ? 'custom-cursor--hover' : ''
+  const pressClass = isPressed ? 'custom-cursor--pressed' : ''
 
   return (
     <>
       <div
-        className={`custom-cursor ${colorClass} ${hoverClass}`}
+        className={`custom-cursor ${colorClass} ${hoverClass} ${pressClass}`}
         style={{
           left: `${position.x}px`,
           top: `${position.y}px`,
         }}
-      />
+      >
+        <span className="custom-cursor__line custom-cursor__line--top" />
+        <span className="custom-cursor__line custom-cursor__line--right" />
+        <span className="custom-cursor__line custom-cursor__line--bottom" />
+        <span className="custom-cursor__line custom-cursor__line--left" />
+        <span className="custom-cursor__dot" />
+      </div>
       <div
         ref={trailElRef}
-        className={`custom-cursor-trail ${colorClass}`}
+        className={`custom-cursor-trail ${colorClass} ${hoverClass} ${pressClass}`}
         style={{
           left: `${trailRef.current.x}px`,
           top: `${trailRef.current.y}px`,
         }}
-      />
+      >
+        <span className="custom-cursor-trail__line custom-cursor-trail__line--top" />
+        <span className="custom-cursor-trail__line custom-cursor-trail__line--right" />
+        <span className="custom-cursor-trail__line custom-cursor-trail__line--bottom" />
+        <span className="custom-cursor-trail__line custom-cursor-trail__line--left" />
+      </div>
     </>
   )
 }
